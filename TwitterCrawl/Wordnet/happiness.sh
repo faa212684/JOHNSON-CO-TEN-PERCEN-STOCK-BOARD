b64efa53d77d362e1a221fcd5fb72b1e025bb6f1
nakul225 <nakul@linuxmint.linuxmint-domain>
2014-06-02 16:37:36 -0400

    Crawled and fetched anger data

---
+++ TwitterCrawl/Wordnet/happiness.sh
@@ -0,0 +1,9 @@
+nice python ./../topsy-crawler.py 1000 10 True happiness/#gladiolus '#gladiolus' > logs/console_log_#gladiolus.txt
+nice python ./../topsy-crawler.py 1000 10 True happiness/#beaming '#beaming' > logs/console_log_#beaming.txt
+nice python ./../topsy-crawler.py 1000 10 True happiness/#sword_lily '#sword_lily' > logs/console_log_#sword_lily.txt
+nice python ./../topsy-crawler.py 1000 10 True happiness/#well-chosen '#well-chosen' > logs/console_log_#well-chosen.txt
+nice python ./../topsy-crawler.py 1000 10 True happiness/#gladiola '#gladiola' > logs/console_log_#gladiola.txt
+nice python ./../topsy-crawler.py 1000 10 True happiness/#felicitous '#felicitous' > logs/console_log_#felicitous.txt
+nice python ./../topsy-crawler.py 1000 10 True happiness/#glad '#glad' > logs/console_log_#glad.txt
+nice python ./../topsy-crawler.py 1000 10 True happiness/#happy '#happy' > logs/console_log_#happy.txt
+
---
+++ TwitterCrawl/Wordnet/surprise.sh
@@ -0,0 +1,10 @@
+nice python ./../topsy-crawler.py 1000 10 True surprise/#force '#force' > logs/console_log_#force.txt
+nice python ./../topsy-crawler.py 1000 10 True surprise/#rage '#rage' > logs/console_log_#rage.txt
+nice python ./../topsy-crawler.py 1000 10 True surprise/#ramp '#ramp' > logs/console_log_#ramp.txt
+nice python ./../topsy-crawler.py 1000 10 True surprise/#surprised '#surprised' > logs/console_log_#surprised.txt
+nice python ./../topsy-crawler.py 1000 10 True surprise/#storm '#storm' > logs/console_log_#storm.txt
+nice python ./../topsy-crawler.py 1000 10 True surprise/#surprise '#surprise' > logs/console_log_#surprise.txt
+nice python ./../topsy-crawler.py 1000 10 True surprise/#surprisal '#surprisal' > logs/console_log_#surprisal.txt
+nice python ./../topsy-crawler.py 1000 10 True surprise/#tempest '#tempest' > logs/console_log_#tempest.txt
+nice python ./../topsy-crawler.py 1000 10 True surprise/#violent_storm '#violent_storm' > logs/console_log_#violent_storm.txt
+
